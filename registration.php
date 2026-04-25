96cf5c455aecb3439b258a2a6e2fdd8d3c096f87
Oliverio Gombert <olivertar@gmail.com>
2026-04-25 18:51:48 +0200

    format and standard

--- registration.php
+++ registration.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the Orangecat PurchaseOrder package.
+ *
+ * (c) Oliverio Gombert <olivertar@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Orangecat_PurchaseOrder',
